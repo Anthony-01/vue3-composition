403e3ea9802995b0a82ff067c998b585acdd848b
Anthony <824283752@qq.com>
2023-03-14 17:49:40 +0800

    eslint调整

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,16 +4,19 @@ module.exports = {
     node: true,
   },
   extends: [
-    "plugin:vue/vue3-essential",
-    "eslint:recommended",
-    "@vue/typescript/recommended",
+    'plugin:vue/vue3-essential',
+    'eslint:recommended',
+    '@vue/typescript/recommended',
   ],
   parserOptions: {
     ecmaVersion: 2020,
   },
   rules: {
-    "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
-    "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
-    "@typescript-eslint/explicit-module-boundary-types": "off",
+    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    quotes: 'off',
+    semi: 'off',
+    'comma-dangle': 'off',
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
   },
 };
